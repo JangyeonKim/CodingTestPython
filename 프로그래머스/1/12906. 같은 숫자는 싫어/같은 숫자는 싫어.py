7e1d0bd0ef882eb85d59506cef040996715a7f63
JangyeonKim <wkddus97@gmail.com>
2024-01-24 22:29:10 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 88.62 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = [arr[0]]
+    
+    for i in range(1, len(arr)) :
+        if answer[-1] == arr[i] :
+            pass
+        else :
+            answer.append(arr[i])
+    
+    return answer
