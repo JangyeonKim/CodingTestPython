d4f9049823b7c2d53b515163ee7e21edc87008ca
JangyeonKim <wkddus97@gmail.com>
2024-02-05 18:58:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,22 +1,19 @@
 import sys
 
-def cal_yaksu(num) :
-    yak_list = []
-    for i in range(1, num+1) :
-        if i**2 > num :
-            break
-        elif num % i == 0 :
-            yak_list.append(i)
-            yak_list.append(int(num/i))
-    return sorted(yak_list)
+input = sys.stdin.readline
 
-a, b = map(int, sys.stdin.readline().strip().split())
+a, b = map(int, input().split())
 
-a_yak = cal_yaksu(a)
-b_yak = cal_yaksu(b)
+a, b = max(a, b), min(a,b)
 
-max_yaksu = max(set(a_yak) & set(b_yak))
-min_baesu = int(a * b / max_yaksu) # 최소공배수 : 두 수의 곱 / 최대공약수
-
-print(max_yaksu)
-print(min_baesu)
+def gcd(a, b) :
+    mod_a = a % b
+    
+    if mod_a == 0 :
+        return b
+    else :
+        return gcd(b, mod_a)
+        
+g = gcd(a, b)
+print(g)
+print(int(a*b/g))
