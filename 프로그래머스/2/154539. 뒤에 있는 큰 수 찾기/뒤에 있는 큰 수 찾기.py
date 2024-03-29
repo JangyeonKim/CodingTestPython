c479b3df3f6a23fc1ba4b7c36e3d8ed12a03b7c6
JangyeonKim <wkddus97@gmail.com>
2024-03-29 15:40:50 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 266.48 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,15 +1,17 @@
-from collections import deque
-
-# stack
+# stack을 이용한 풀이
+# queue를 사용해서 2중 반복문으로 작성하였을 경우 시간 초과
 def solution(numbers):
-    stack = []
     answer = [0] * len(numbers)
-
-    for i in range(len(numbers)):
-            while stack and numbers[stack[-1]] < numbers[i]:
-                answer[stack.pop()] = numbers[i]
-            stack.append(i)
-    while stack:
-            answer[stack.pop()] = -1
+    stack = []
     
+    # 1. for문 : 뒤에있는 큰 수가 있는 경우에 대한 처리
+    for i in range(len(numbers)) :
+        while stack and numbers[stack[-1]] < numbers[i] :# stack[-1] 인덱스에 해당하는 numbers의 수보다 numbers[i]가 더 크면. 즉, 뒷큰수이면 
+            answer[stack.pop()] = numbers[i] # 해당 index에 대해 뒷큰수 할당
+        stack.append(i)
+    
+    # 2. while문 : 남은 부분들에 대해 -1 처리
+    while stack :
+        answer[stack.pop()] = -1
+        
     return answer
