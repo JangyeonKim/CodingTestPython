ef6eae119898aa389fb7e30a956cb4f7fd940d91
JangyeonKim <wkddus97@gmail.com>
2024-04-07 14:20:32 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,18 @@
 def solution(n, computers):
     answer = 0
-    visited = [False for _ in range(n)]     # 방문 여부 저장 배열
+    visited = [False for _ in range(n)]
     
-    for com in range(n):
-        if visited[com] == False:   # 1번 ~ n번 컴퓨터까지 가면서 방문하지 않은 컴퓨터라면
-            DFS(n, computers, com, visited)
-            answer += 1
+    for com in range(n) :
+        if not visited[com] :
+            dfs(n, computers, com, visited)
+            answer+= 1 # dfs로 컴퓨터들을 최대한 방문후 빠져나오면 하나의 네트워크
+    
     return answer
 
-
-def DFS(n, computers, com, visited):
-    visited[com] = True        # 해당 노드의 방문 여부를 True로 변경
-    for i in range(n):          # 해당 노드와 연결된 다음 노드들을 모두 탐색
-        if i != com and computers[i][com] == 1 and visited[i] == False:    
-                DFS(n, computers, i, visited)       # i랑 연결된 다음 노드도 쭉 탐색
-
-# 자기 자신이 아니고, 이전 노드(com)와 현재 노드(i)가 연결된 노드이며, 현재 노드가 방문하지 않은 노드라면 방문하고, 현재 노드(i)에 대한 DFS를 계속 진행
-# 연결되지 않은 노드가 나오는 순간 DFS 종료
+def dfs(n, computers, com, visited) :
+    visited[com] = True # 해당 컴퓨터 방문처리
+    
+    for connect in range(n) :
+        if connect != com and computers[connect][com] == 1 :
+            if not visited[connect] :
+                dfs(n, computers, connect, visited)
