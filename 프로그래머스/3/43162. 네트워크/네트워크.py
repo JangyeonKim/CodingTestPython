9a31f74e08d006389ddb4ade28d8c2257a492a0f
JangyeonKim <wkddus97@gmail.com>
2024-04-08 11:08:48 +0900

    [level 3] Title: 네트워크, Time: 1.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -4,7 +4,7 @@ def solution(n, computers):
     
     for com in range(n) :
         if not visited[com] :
-            dfs(n, computers, com, visited)
+            bfs(n, computers, com, visited)
             answer+= 1 # dfs로 컴퓨터들을 최대한 방문후 빠져나오면 하나의 네트워크
     
     return answer
@@ -16,3 +16,14 @@ def dfs(n, computers, com, visited) :
         if connect != com and computers[connect][com] == 1 :
             if not visited[connect] :
                 dfs(n, computers, connect, visited)
+                
+def bfs(n, computers, com, visited):
+    queue = []
+    queue.append(com)
+    while len(queue) != 0:
+        com = queue.pop(0)
+        visited[com] = True
+        for connect in range(n):
+            if connect != com and computers[com][connect] == 1:
+                if visited[connect] == False:
+                    queue.append(connect)
