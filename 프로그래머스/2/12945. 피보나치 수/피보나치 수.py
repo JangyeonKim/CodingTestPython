0b12820d00df47c4125a069e8ff8653acd0d5d7d
JangyeonKim <wkddus97@gmail.com>
2024-08-27 16:21:19 +0900

    [level 2] Title: 피보나치 수, Time: 421.95 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,8 @@
 def solution(n):
-    pibo = [0] * (n+1)
+    pibo = [0, 1] + [0] * (n-1)
     
-    pibo[0] = 0
-    pibo[1] = 1
-    
-    for i in range(2, n+1) :
+    for i in range(2, len(pibo)) :
         pibo[i] = pibo[i-1] + pibo[i-2]
     
-    return pibo[-1] % 1234567
+    answer = pibo[-1] % 1234567
+    return answer 
