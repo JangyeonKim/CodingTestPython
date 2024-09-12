9b7db50fa93f78c20555a114f8c9de831d1fa2ad
JangyeonKim <wkddus97@gmail.com>
2024-09-12 15:48:12 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,15 +1,13 @@
 def solution(s):
-
-    S = s.lower()
-    H = S.split(' ')
-    STRING = []
-
-    for i in H:
-        if i:
-            Z = i[0].upper() + i[1:]
-        else:
-            Z = i
-        STRING.append(Z)
-    last = ' '.join(STRING)
-
-    return last
+    s_list = s.lower().split(" ")
+    # print(s_list)
+    
+    answer=[]
+    for string in s_list :
+        if string == "" or string[0].isdigit():
+            answer.append(string)
+        elif string[0].isalpha() :
+            answer.append(string[0].upper() + string[1:])
+    
+    
+    return " ".join(answer)
