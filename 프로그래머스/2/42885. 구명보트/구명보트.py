52b906619a491e60c8002b56a82754d8f0b8c1e4
JangyeonKim <wkddus97@gmail.com>
2024-09-13 11:12:38 +0900

    [level 2] Title: 구명보트, Time: 9.18 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,19 @@
 def solution(people, limit):
-    people.sort()  # 사람들의 몸무게를 오름차순으로 정렬
-    left = 0  # 가장 가벼운 사람을 가리키는 포인터
-    right = len(people) - 1  # 가장 무거운 사람을 가리키는 포인터
     boats = 0  # 필요한 구명보트의 수
-
-    while left <= right:
-        # 가장 가벼운 사람과 가장 무거운 사람의 합이 제한을 넘지 않으면 함께 태운다
-        if people[left] + people[right] <= limit:
-            left += 1  # 가장 가벼운 사람 태웠으므로 포인터 이동
-        # 가장 무거운 사람은 항상 보트에 태운다
-        right -= 1  # 가장 무거운 사람 태웠으므로 포인터 이동
-        boats += 1  # 보트 하나 사용
+    
+    people.sort(reverse=True)
+    
+    i= 0 
+    j = len(people)-1
+    
+    while i <= j :
+        # print(i, j)
+        if people[i] + people[j] <= limit :
+            i+=1
+            j-=1
+            boats += 1
+        else :
+            i+=1
+            boats += 1
 
     return boats
