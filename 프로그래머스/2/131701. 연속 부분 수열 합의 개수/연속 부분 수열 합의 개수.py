d2d480d7a81d23619f3d6ae892528b0630210327
JangyeonKim <wkddus97@gmail.com>
2024-09-13 12:38:03 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4539.84 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,21 +1,14 @@
 def solution(elements):
-    ans_list = []
+    sum_arr = elements + [sum(elements)]
+    # print(sum_arr)
     
-    for win_len in range(1,len(elements)+1) : # 1~len(elements)
-        if win_len == 1 :
-            ans_list.append(elements)
-        elif win_len == len(elements) :
-            ans_list.append([sum(elements)])
-        else :
-            ele = elements[-(win_len-1):] + elements
-            temp = []
-
-            for i in range(len(elements)) :
-                t = sum(ele[i:i+win_len])
-                temp.append(t)
-            ans_list.append(temp)
+    for i in range(2, len(elements)) :
+        arr = elements + elements[:i-1]
+        # print(arr)
+        
+        for j in range(len(elements)) :
+            sum_arr.append(sum(arr[j:j+i]))
+            
+    answer = len(set(sum_arr))
     
-    ans = ans_list.pop()
-    for a in ans_list :
-        ans += a
-    return len(set(ans))
+    return answer
