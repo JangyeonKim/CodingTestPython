8d21c5224363916d2d00bb6079d35d7678af11cb
JangyeonKim <wkddus97@gmail.com>
2024-09-24 17:11:12 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,28 +1,22 @@
 from collections import deque
 
-def discriminate(process, queue) :
-    for q in queue :
-        if q[0] > process[0] :
-            return False # 우선순위가 더 높은 프로세스가 있으면 false 리턴
-    return True # 없으면 true 리턴
-
+def check(c, arr) :
+    for a in arr :
+        if a[0] > c[0] :
+            return False
+    return True
 
 def solution(priorities, location):
     answer = 0
+    pri = [[p, x] for x, p in enumerate(priorities)]
     
-    queue = [[x, idx] for idx, x in enumerate(priorities)]
-    queue = deque(queue)
-    
-    while True :
-        process = queue.popleft()
-        
-        if discriminate(process, queue) :
-            answer += 1
-            if process[1] == location :
-                return answer
+    while pri :        
+        cur = pri.pop(0)
+        if not check(cur, pri) :
+            pri.append(cur)
         else :
-            queue.append(process)
-                
-    
-    
-    return answer
+            answer += 1
+            if cur[1] == location :
+                return answer
+            
+        
