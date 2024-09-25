44c0e14e632085d88bc34c236c7c7b076d0f2c6f
JangyeonKim <wkddus97@gmail.com>
2024-09-25 18:55:20 +0900

    [level 2] Title: 타겟 넘버, Time: 169.91 ms, Memory: 49.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,34 +1,11 @@
-# 번호 하나 당 2개(+,-)로 2^len(numbers) 경우의 수 존재
-
-# 1. bfs
-def bfs_solution(numbers, target):
-    ans_list = [0]
-    
-    for i in range(len(numbers)):
-        temp = []
-        for a in ans_list :
-            temp.append(a+numbers[i])
-            temp.append(a-numbers[i])
-        ans_list = temp
-            
-    return ans_list.count(target)
-
-# 2. dfs
-def dfs(numbers, target, idx, value) :
-    global cnt
-    
-    if idx == len(numbers) and value == target :
-        cnt+=1
-        return
-    elif idx == len(numbers) :
-        return
-    
-    dfs(numbers, target, idx+1, value + numbers[idx])
-    dfs(numbers, target, idx+1, value - numbers[idx])
-
 def solution(numbers, target):
-    global cnt
-    cnt = 0
+    answer = [0]
     
-    dfs(numbers, target, 0, 0)
-    return cnt
+    for n in numbers :
+        temp = []
+        for ans in answer :
+            temp.append(ans + n)
+            temp.append(ans - n)
+        answer = temp
+    
+    return answer.count(target)
