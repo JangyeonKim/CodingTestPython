5bee38ccbaf93ae60f693b1a6e35b09d506d2b05
JangyeonKim <wkddus97@gmail.com>
2024-09-26 20:25:16 +0900

    [level 2] Title: 땅따먹기, Time: 174.71 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,7 +1,10 @@
 def solution(land):
+    answer = 0
     
     for i in range(1, len(land)) :
         for j in range(4) :
-            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
-    
+            temp_land = land[i-1].copy()
+            temp_land.pop(j)
+            land[i][j] += max(temp_land)
+            
     return max(land[-1])
