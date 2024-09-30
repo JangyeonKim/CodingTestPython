8e420fe471cd65c54dd1547fec079847ecf3f1a1
JangyeonKim <wkddus97@gmail.com>
2024-09-30 20:47:46 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.87 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,9 +1,10 @@
 def solution(n):
+    if n <= 2:
+        return n
     
-    ans_list = [0 for _ in range(n+1)]
-    ans_list[0], ans_list[1] = 1, 1
-
-    for i in range(2, n+1) :
-        ans_list[i] = (ans_list[i-1] + ans_list[i-2]) % 1000000007
+    num = [1, 2] + [0] * (n-2)
     
-    return ans_list[-1] 
+    for i in range(2, n) :
+        num[i] = (num[i-1] + num[i-2])% 1000000007
+    
+    return num[-1] 
