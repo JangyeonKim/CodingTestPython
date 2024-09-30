613316ad9acdf1fb11e96f72a28307221c330ddd
JangyeonKim <wkddus97@gmail.com>
2024-09-30 19:47:40 +0900

    [level 2] Title: 택배상자, Time: 259.07 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -2,9 +2,9 @@ def solution(order):
     stack = []
     length = len(order)
     index = 0
-    
     for num in range(1, length + 1):
         stack.append(num)
+
         while stack and stack[-1] == order[index]:
             stack.pop()
             index += 1
