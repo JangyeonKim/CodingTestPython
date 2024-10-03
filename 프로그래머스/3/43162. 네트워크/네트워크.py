715b36c8d451f8dc546fca6a8d0ae8a2d6b0131c
JangyeonKim <wkddus97@gmail.com>
2024-10-03 17:36:12 +0900

    [level 3] Title: 네트워크, Time: 0.58 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,29 +1,19 @@
+def dfs(visited, com, computers) :
+    for c in range(len(computers[com])) :
+        if computers[com][c] == 1 and not visited[c] :
+            visited[c] = True
+            dfs(visited, c, computers)
+    
+
 def solution(n, computers):
     answer = 0
-    visited = [False for _ in range(n)]
+    
+    visited = [False] * n
     
     for com in range(n) :
-        if not visited[com] :
-            bfs(n, computers, com, visited)
-            answer+= 1 # dfs로 컴퓨터들을 최대한 방문후 빠져나오면 하나의 네트워크
+        if visited[com] == False :
+            visited[com] = True
+            answer += 1
+            dfs(visited, com, computers)
     
     return answer
-
-def dfs(n, computers, com, visited) :
-    visited[com] = True # 해당 컴퓨터 방문처리
-    
-    for connect in range(n) :
-        if connect != com and computers[connect][com] == 1 :
-            if not visited[connect] :
-                dfs(n, computers, connect, visited)
-                
-def bfs(n, computers, com, visited):
-    queue = []
-    queue.append(com)
-    while len(queue) != 0:
-        com = queue.pop(0)
-        visited[com] = True
-        for connect in range(n):
-            if connect != com and computers[com][connect] == 1:
-                if visited[connect] == False:
-                    queue.append(connect)
