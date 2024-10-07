4099b4ac17fd26b9304c12725690fed2bbeaf9af
JangyeonKim <wkddus97@gmail.com>
2024-10-07 20:14:34 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,12 @@
 def solution(n):
-    if n < 3 :
+    
+    if n <= 2 :
         return n
     
-    n_list = [0 for _ in range(n)]
-    n_list[0] = 1
-    n_list[1] = 2
+    arr = [1, 2] + [0]*(n-2)
     
-    for i in range(2, n) :
-        n_list[i] = n_list[i-1] + n_list[i-2]
+    for i in range(2, len(arr)) :
+        arr[i] = arr[i-2] + arr[i-1]
     
-    return n_list[-1] % 1234567
+    
+    return arr[-1] % 1234567
