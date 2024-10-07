0761f1beb39bec33560c02a0ff8068f23533c187
JangyeonKim <wkddus97@gmail.com>
2024-10-07 19:41:20 +0900

    [level 2] Title: 구명보트, Time: 8.91 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,14 @@
+from collections import deque
 def solution(people, limit):
-    boats = 0  # 필요한 구명보트의 수
+    answer = 0
     
-    people.sort(reverse=True)
+    people.sort()
+    people = deque(people)
     
-    i= 0 
-    j = len(people)-1
+    while people :
+        pig = people.pop()
+        if people and pig + people[0] <= limit :
+            people.popleft()
+        answer += 1
     
-    while i <= j :
-        # print(i, j)
-        if people[i] + people[j] <= limit :
-            i+=1
-            j-=1
-            boats += 1
-        else :
-            i+=1
-            boats += 1
-
-    return boats
+    return answer
