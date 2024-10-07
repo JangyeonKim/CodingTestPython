3b73d80fc8889726fe4d7be434f343ebc4d5c11a
JangyeonKim <wkddus97@gmail.com>
2024-10-07 20:53:59 +0900

    [level 2] Title: 할인 행사, Time: 204.33 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,27 +1,25 @@
 from collections import Counter
 
-def same(dict1, dict2) :
+def canBuy(dict1, dict2) :
     for key in dict1.keys() :
-        if dict1[key] != dict2[key] :
+        if key not in dict2.keys() :
             return False
-    
+        elif dict1[key] != dict2[key] :
+            return False
     return True
 
 def solution(want, number, discount):
     answer = 0
     
-    w_dict = {}
+    want_dict = dict()
     
     for w, n in zip(want, number) :
-        w_dict[w] = n
+        want_dict[w] = n
         
-    cal_days = len(discount) - 10
-    
-    for i in range(cal_days+1) :
-        days_discount = discount[i:i+10]
-        d_dict = Counter(days_discount)
-        
-        if same(w_dict, d_dict) :
+    for i in range(len(discount)-9) :
+        temp_discount = discount[i:i+10]
+        temp_dict = Counter(temp_discount)
+        if canBuy(want_dict, temp_dict) :
             answer += 1
     
     return answer
