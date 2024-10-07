ab1d5387573cd5d64ba920a7a730224a28b94744
JangyeonKim <wkddus97@gmail.com>
2024-10-07 15:56:43 +0900

    [level 2] Title: 숫자의 표현, Time: 3.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,17 +1,15 @@
 def solution(n):
     answer = 0
     
-    half = n//2 + 1
-    
-    for i in range(1, half) :
-        temp = 0
-        
-        j = i
-        while temp < n :
-            temp += j
-            j += 1
-        
-        if temp == n :
-            answer += 1
+    for i in range(1, n//2 + 1) :
+        ans = 0
+        while True :
+            ans += i
+            i+= 1
+            if ans > n :
+                break
+            elif ans == n :
+                answer += 1
+                break
     
     return answer+1
