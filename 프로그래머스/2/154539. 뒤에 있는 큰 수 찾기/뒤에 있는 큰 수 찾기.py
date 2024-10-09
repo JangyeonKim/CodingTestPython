3c0f89b129c8d27e832dff4529ccc6542ee3e42d
JangyeonKim <wkddus97@gmail.com>
2024-10-09 21:04:39 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 335.32 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,14 +1,13 @@
 def solution(numbers):
     answer = [0] * len(numbers)
-    
     stack = []
+    
     for i in range(len(numbers)) :
-        while stack and numbers[stack[-1]] < numbers[i] : # 오큰수 발견
-            answer[stack.pop()] = numbers[i] # 오큰수 할당
+        while stack and numbers[i] > numbers[stack[-1]] :
+            answer[stack.pop()] = numbers[i]
+        stack.append(i)
         
-        stack.append(i) # 오큰수 할당 대기
-        
-    while stack : # 오큰수 없는 자리에 -1 할당
-        answer[stack.pop()] = -1
+    for i in stack :
+        answer[i] = -1
     
     return answer
