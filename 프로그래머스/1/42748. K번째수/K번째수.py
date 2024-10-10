67c1895b4d980d90fe13e3627bc84ca928106dd6
JangyeonKim <wkddus97@gmail.com>
2024-10-10 20:25:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,11 @@
 def solution(array, commands):
     answer = []
     
-    for c in commands :
-        i, j, k = c[0], c[1], c[2]
+    for s, e, i in commands :
+        s-= 1
+        i-=1
         
-        temp_list = array[i-1 : j]
-        answer.append(sorted(temp_list)[k-1])
+        temp = array[s:e]
+        answer.append(sorted(temp)[i])
     
     return answer
