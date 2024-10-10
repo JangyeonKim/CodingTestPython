773129a5aa7507be714cb9a9ba56d5f9a9219d15
JangyeonKim <wkddus97@gmail.com>
2024-10-10 19:03:27 +0900

    [level 3] Title: 단속카메라, Time: 0.95 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,12 +1,15 @@
+from collections import deque
+
 def solution(routes):
-    answer = 1
+    answer = 0
     
-    routes.sort(key=lambda x: x[1])  # 끝나는 지점을 기준으로 정렬
-    camera = routes[0][1]  # 첫 번째 차량의 나가는 지점에 카메라 설치
-    
-    for r in routes:
-        if r[0] > camera:  # 카메라가 해당 경로를 포함하지 않으면
-            camera = r[1]  # 새로운 카메라 설치
+    routes.sort(key = lambda x : x[1])
+    camera = -30001
+    for in_, out_ in routes :
+        if in_ <= camera <= out_ :
+            pass
+        else :
+            camera = out_
             answer += 1
-    
+     
     return answer
