c2e4195c9fc7827daa0b402e958aa7058e84cca9
JangyeonKim <wkddus97@gmail.com>
2024-10-10 17:00:29 +0900

    [level 3] Title: 단어 변환, Time: 5.71 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,31 +1,28 @@
 from collections import deque
 
-def count_diff(word1, word2) :
+def canTransform(word1, word2) :
     cnt = 0
     for w1, w2 in zip(word1, word2) :
         if w1 != w2 :
             cnt += 1
-            
-    return True if cnt==1 else False
+    return True if cnt == 1 else False
 
-def bfs(begin, target, words, answer) :
-    queue = deque([[begin, answer]])
-    check = []
-    
+def bfs(queue, words, target) :
+    answer = 0
     while queue :
-        word, answer = queue.popleft()
+        word, cnt = queue.popleft()
         if word == target :
-            return answer
-        for w in words :
-            if w not in check and count_diff(word, w) :
-                queue.append([w, answer+1])
-                check.append(w)
-    return 0
-            
+            return cnt
+        for wrd in words :
+            if canTransform(word, wrd) :
+                queue.append([wrd, cnt+1])
+        
+
 def solution(begin, target, words):
     if target not in words :
         return 0
     
-    answer = bfs(begin, target, words, 0)
-    
-    return answer 
+    queue = deque([[begin, 0]])
+    answer = bfs(queue, words, target)
+        
+    return answer
