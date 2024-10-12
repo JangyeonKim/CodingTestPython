e3b4af1084dbbe46bb636d486710c55fa9e80719
JangyeonKim <wkddus97@gmail.com>
2024-10-12 16:23:18 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.62 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,29 +1,32 @@
-import heapq
+from heapq import heapify, heappop, heappush
+
 def solution(jobs):
     answer = 0
     length = len(jobs)
-    heapq.heapify(jobs)
-    time = 0
     heap = []
-    while(jobs):           
-        while(jobs):      #그때그때 시간에 따라 가능한 목록을 힙에 넣고
-            x,y= heapq.heappop(jobs)
-            if x>time:
-                heapq.heappush(jobs,[x,y])
+    heapify(jobs)
+    time = 0
+    
+    while jobs :
+        while jobs :
+            offer, consume = heappop(jobs)
+            
+            if offer > time :
+                heappush(jobs, [offer, consume])
                 break
-            else:
-                heapq.heappush(heap,[y,x])
-
-        if len(heap)==0:      #힙에 아무것도 없으면 time 증가, 있다면 pop한다
+            else :
+                heappush(heap, [consume, offer])
+        
+        if len(heap) == 0 :
             time += 1
-        else:
-            a,b = heapq.heappop(heap)
-            answer += (a+time-b)
-            time += a
-    while heap:              # 남아 있는 힙 순서대로 pop
-        a,b = heapq.heappop(heap)
-        answer += a+time-b
-        time += a
-        print(time)
-
-    return answer//length
+        else :
+            consume, offer = heappop(heap)
+            answer += consume+time-offer
+            time += consume
+        
+    while heap : 
+        consume, offer = heappop(heap)
+        answer += consume+time-offer
+        time += consume
+    
+    return answer // length
