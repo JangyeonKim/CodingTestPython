de67843002d23ea7919379739b0e50c4d4d1f0dd
JangyeonKim <wkddus97@gmail.com>
2025-02-24 17:04:14 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,18 +1,31 @@
-N = input()
+N = int(input())
 
-ans_list = []
+#ans_list = []
+#
+#for i in range(1, int(N)) :
+#    str_i = str(i)
+#    
+#    temp = i
+#    for s in str_i :
+#        temp += int(s)
+#    
+#    if temp == int(N) :
+#        ans_list.append(i)
+#        break
+#        
+#print(ans_list[0] if ans_list else 0)
 
-for i in range(1, int(N)) :
-    str_i = str(i)
-    
+ans = 0
+
+start = max(1, N - 9 * len(str(N))) # 탐색 범위 줄임
+
+for i in range(start, N) :
     temp = i
-    for s in str_i :
+    
+    for s in str(i) :
         temp += int(s)
     
-    if temp == int(N) :
-        ans_list.append(i)
+    if temp == N :
+        ans = i
         break
-        
-print(ans_list[0] if ans_list else 0)
-
-    
+print(ans)
