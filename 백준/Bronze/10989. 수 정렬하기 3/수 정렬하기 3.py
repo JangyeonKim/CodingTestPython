54dbf7d8271684dfd2eea2ac26eff83fcd01a49e
JangyeonKim <wkddus97@gmail.com>
2025-02-25 12:55:51 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8168 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = [0 for _ in range(10000+1)]
+
+for _ in range(N) :
+    arr[int(input())] += 1
+    
+for i in range(len(arr)) :
+    if arr[i] != 0 :
+        for _ in range(arr[i]) :
+            print(i)
+        
