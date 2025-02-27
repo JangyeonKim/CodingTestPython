6e0a1c1c9381d39ffea6f5ac56bad811a6b50550
JangyeonKim <wkddus97@gmail.com>
2025-02-27 11:12:35 +0900

    [Silver III] Title: 1로 만들기, Time: 1392 ms, Memory: 200364 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+x = int(input())
+
+# x -> 1
+
+target = 1 
+cnt = 0
+queue = deque([(x, cnt)])
+
+while queue :
+    xx, c = queue.popleft()
+
+    if xx == 1 :
+        print(c)
+        break
+    
+    if xx % 3 == 0 :
+        queue.append((xx//3, c+1))
+    if xx % 2 == 0 :
+        queue.append((xx//2, c+1))
+    if xx >= 1 :
+        queue.append((xx-1, c+1))
