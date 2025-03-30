741034fe5ce8e2a158c2c1d8368f0b51582bd7e3
JangyeonKim <wkddus97@gmail.com>
2025-03-30 19:41:19 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,37 +1,37 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
+M = int(input())
 
-n_com = int(input())
-n_link = int(input())
-
-arr = [[] for _ in range(n_com+1)]
-
-for _ in range(n_link) :
+graph = [[] for _ in range(N+1)]
+for _ in range(M) :
     a, b = map(int, input().split())
-    arr[a].append(b)
-    arr[b].append(a)
-
-visit = [False] * (n_com+1)
-
-from collections import deque
-
-def bfs(start, visit, arr) :
-    queue = deque(arr[start]) # 1번 컴퓨터
-    visit[start] = True
-    cnt = 0
+    graph[a].append(b)
+    graph[b].append(a)
     
-    while queue : 
-        com = queue.popleft()
+# from collections import deque 
+# visit = [False] * (N+1)
+# def bfs(start, visit, graph) :
+#     queue = deque([start])
+#     cnt = -1
+    
+#     while queue :
+#         q = queue.popleft()
+#         if not visit[q] :
+#             visit[q] = True
+#             cnt += 1
+#             for g in graph[q] :
+#                 if not visit[g] :
+#                     queue.append(g)
+#     return cnt
+# print(bfs(1, visit, graph))
 
-        if not visit[com] :
-            visit[com] = True
-            cnt += 1
-
-            for c in arr[com] :
-                if not visit[c] :
-                    queue.append(c)
-
-    return cnt
-
-answer = bfs(1, visit, arr)
-print(answer)
+visit = [False] * (N+1)
+cnt = -1
+def dfs(start, visit, graph) :
+    global cnt
+    visit[start] = True
+    cnt += 1
+    for g in graph[start] :
+        if not visit[g] :
+            dfs(g, visit, graph)
+dfs(1, visit, graph)    
+print(cnt)
